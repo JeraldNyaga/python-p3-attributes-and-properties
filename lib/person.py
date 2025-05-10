dcfb96657de0e909750def08bd352f4b2dbb884b
judgeOstrich <jeraldnyaga15@gmail.com>
2025-05-10 18:33:42 +0300

    Add attributes and properties

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,26 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Mark", job="Sales"):
+        self.name = name
+        self.job = job
+    def set_name(self,name):
+        if(type(name) == str) and (0 < len(name)< 26):
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def set_job(self, job):
+        if(job in APPROVED_JOBS):
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    def get_job(self):
+        return self._job
+    def get_name(self):
+        return self._name
+
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+    
